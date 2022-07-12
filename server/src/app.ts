84f85836829ac7f6a742aa1c7e9d6928029e3883
ajrfyd <ajrfyd@naver.com>
2022-07-12 18:01:52 +0900

    Modify: Modify PORT Num

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,12 +2,14 @@ import express from 'express';
 
 const app = express();
 
+const PORT = process.env.PORT || 8000;
+
 app.use(express.static('build'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/build/index.html');
 });
 
-app.listen(8000, () => {
+app.listen(PORT, () => {
   console.log('Server listening on 8000');
 })
